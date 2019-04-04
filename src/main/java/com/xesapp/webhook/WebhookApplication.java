bd91534cf6149bca045d40b6a10ba66576e1d8a9
zhangdejun <zhangdejun@100tal.com>
2019-04-04 17:42:16 +0800

    add async

--- src/main/java/com/xesapp/webhook/WebhookApplication.java
+++ src/main/java/com/xesapp/webhook/WebhookApplication.java
@@ -2,9 +2,12 @@ package com.xesapp.webhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 /**
  * @author tal_zhuyuehui 2019-04-04
  */
+@EnableAsync
 @SpringBootApplication
 public class WebhookApplication {
 
